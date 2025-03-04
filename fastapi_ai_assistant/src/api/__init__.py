71b20934a1e74fd8007b70d25744f0af5cd61a3e
Vitaly D <vitcode.mail@gmail.com>
2025-03-04 15:41:49 +0300

    Chat2 example added (Depends and other)

--- fastapi_ai_assistant/src/api/__init__.py
+++ fastapi_ai_assistant/src/api/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from api.v1 import chat_1
+from api.v1 import chat_1, chat_2
 
 router = APIRouter()
 
@@ -8,5 +8,11 @@ router = APIRouter()
 router.include_router(
     chat_1.router,
     prefix="/v1/chat1",
+    tags=["ChatExample"],
+)
+
+router.include_router(
+    chat_2.router,
+    prefix="/v1/chat2",
     tags=["ChatExample"],
 )
